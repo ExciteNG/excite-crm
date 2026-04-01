b16a0621a626f8aca0ca83637a9a6e50d426afdf
Oludire Damilare <oludire@gmail.com>
2026-04-01 12:01:23 +0100

    refactor: enhance user and lead filtering with status selection and improve status badge logic

--- src/components/dashboardLayout/AllUsers.tsx
+++ src/components/dashboardLayout/AllUsers.tsx
@@ -24,64 +24,64 @@ import {
   SelectValue,
 } from "../ui/select";
 import StatusBadge from "../dashboardUI/reusableComponents/StatusBadge";
-import PaginationComponent from "../dashboardUI/reusableComponents/Pagination";
+import { Label } from "@/src/components/ui/label";
 
-const userss = [
-  {
-    fullname: "Jenny Wilson",
-    email: "john@church.com",
-    phoneNumber: "0819 012 3456",
-    location: { lga: "Yaba", state: "lagos" },
-    source: "Facebook",
-    lastLogin: "2 min ago",
-    status: "Active",
-  },
-  {
-    fullname: "Eleanor Pena",
-    email: "john@church.com",
-    phoneNumber: "0901 123 4567",
-    location: { lga: "Mushin", state: "lagos" },
-    source: "Tiktok",
-    lastLogin: "1 hour ago",
-    status: "Active",
-  },
-  {
-    fullname: "Leslie Alexander",
-    email: "john@church.com",
-    phoneNumber: "0704 567 8901",
-    location: { lga: "Ajegunle", state: "lagos" },
-    source: "Youtube",
-    lastLogin: "2 hour ago",
-    status: "Active",
-  },
-  {
-    fullname: "Marvin McKinney",
-    email: "john@church.com",
-    phoneNumber: "0810 123 4567",
-    location: { lga: "Computer Village", state: "lagos" },
-    source: "Whatsapp",
-    lastLogin: "8 hour ago",
-    status: "Active",
-  },
-  {
-    fullname: "Arlene McCoy",
-    email: "john@church.com",
-    phoneNumber: "0701 234 5678",
-    location: { lga: "Abule Egba", state: "lagos" },
-    source: "Online Event",
-    lastLogin: "1 day ago",
-    status: "Active",
-  },
-  {
-    fullname: "Albert Flores",
-    email: "john@church.com",
-    phoneNumber: "0817 890 1234",
-    location: { lga: "Eko Hotel", state: "lagos" },
-    source: "Others",
-    lastLogin: "6 months",
-    status: "Inactive",
-  },
-];
+// const users = [
+//   {
+//     fullname: "Jenny Wilson",
+//     email: "john@church.com",
+//     phoneNumber: "0819 012 3456",
+//     location: { lga: "Yaba", state: "lagos" },
+//     source: "Facebook",
+//     lastLogin: "2 min ago",
+//     status: "Active",
+//   },
+//   {
+//     fullname: "Eleanor Pena",
+//     email: "john@church.com",
+//     phoneNumber: "0901 123 4567",
+//     location: { lga: "Mushin", state: "lagos" },
+//     source: "Tiktok",
+//     lastLogin: "1 hour ago",
+//     status: "Active",
+//   },
+//   {
+//     fullname: "Leslie Alexander",
+//     email: "john@church.com",
+//     phoneNumber: "0704 567 8901",
+//     location: { lga: "Ajegunle", state: "lagos" },
+//     source: "Youtube",
+//     lastLogin: "2 hour ago",
+//     status: "Active",
+//   },
+//   {
+//     fullname: "Marvin McKinney",
+//     email: "john@church.com",
+//     phoneNumber: "0810 123 4567",
+//     location: { lga: "Computer Village", state: "lagos" },
+//     source: "Whatsapp",
+//     lastLogin: "8 hour ago",
+//     status: "Active",
+//   },
+//   {
+//     fullname: "Arlene McCoy",
+//     email: "john@church.com",
+//     phoneNumber: "0701 234 5678",
+//     location: { lga: "Abule Egba", state: "lagos" },
+//     source: "Online Event",
+//     lastLogin: "1 day ago",
+//     status: "Active",
+//   },
+//   {
+//     fullname: "Albert Flores",
+//     email: "john@church.com",
+//     phoneNumber: "0817 890 1234",
+//     location: { lga: "Eko Hotel", state: "lagos" },
+//     source: "Others",
+//     lastLogin: "6 months",
+//     status: "Inactive",
+//   },
+// ];
 
 export type UserStatus = "dormant" | "pending" | "active";
 
@@ -105,96 +105,101 @@ const AllUsers = () => {
     }
   });
 
-  // console.log(filteredUsers);
+  console.log(filteredUsers);
 
   return (
-    <div className="rounded-2xl bg-white p-6 shadow-sm">
-      <div className="mb-4 flex items-center justify-between">
-        <div>
-          <h2 className="text-lg font-semibold">All Users</h2>
-          <p className="text-muted-foreground text-sm">
-            Registered users and their activity status
-          </p>
+    <>
+    <div className="rounded-sm bg-background p-6 shadow-sm">
+       <div className="flex justify-between w-full ">
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col w-full">
+            <h2 className="text-lg font-semibold">Users</h2>
+            <p className="text-muted-foreground text-sm font-light">
+              Registered users and activity status
+            </p>
+          </div>
         </div>
 
-        <Select
-          value={status}
-          onValueChange={(value) => setStatus(value as UserStatus | "all")}
-        >
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="Filter by status" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectLabel>Status</SelectLabel>
-              {statusOptions.map((status) => (
-                <SelectItem key={status} value={status} className="capitalize">
-                  {status.charAt(0).toUpperCase() +
-                    status.slice(1).toLowerCase()}
+        <div className="flex flex-col items-end gap-1 my-2.5">
+          <Label className="w-[180px]">Filter by Status</Label>
+          <Select
+            value={status}
+            onValueChange={(value) => setStatus(value as UserStatus | "all")}
+          >
+            <SelectTrigger
+              className="ml-auto h-7 w-[180px] rounded-lg pl-2.5"
+              aria-label="Select a value"
+            >
+              <SelectValue placeholder="All Status" />
+            </SelectTrigger>
+            <SelectContent className="rounded-xl">
+              {statusOptions.map((status, index) => (
+                <SelectItem value={status} key={index} className="capitalize">
+                  {status.charAt(0).toUpperCase() + status.slice(1)}
                 </SelectItem>
               ))}
-            </SelectGroup>
-          </SelectContent>
-        </Select>
+            </SelectContent>
+          </Select>
+        </div>
       </div>
-
-      {/* Table */}
-      <div className="space-y-5 divide-y-2 divide-solid">
-        <Table>
-          <TableHeader className="sticky top-0">
-            <TableRow className="bg-muted/50">
+      <div className="max-h-[300px] h-full overflow-y-auto relative">
+        <Table className="w-full">
+          <TableHeader>
+            <TableRow>
               {userManagementTableHeader.map((header: string) => (
                 <TableHead
                   key={header}
-                  className="text-center text-[#4F4F4F] capitalize"
+                  className="sticky top-0 z-10 bg-secondary capitalize text-center text-primary-foreground"
                 >
                   {header}
                 </TableHead>
               ))}
             </TableRow>
           </TableHeader>
-          <TableBody className="">
-            {filteredUsers?.map((user, index) => {
-              return (
-                <TableRow key={index} className="text-center">
-                  <TableCell className="flex w-fit items-center gap-2.5">
-                    <div className="bg-primary/10 border-primary w-fit rounded-full border p-2">
-                      <UserIcon size={16} className="text-green-600" />
-                    </div>
-                    <div>
-                      <p className="font-medium">{user.fullname}</p>
-                      <p className="font-light">{user.email}</p>
-                    </div>
-                  </TableCell>
 
-                  <TableCell>{user.phoneNumber}</TableCell>
-                  <TableCell>
-                    {user.location.lga} {user.location?.state}
-                  </TableCell>
-                  <TableCell>{user.source || "-"}</TableCell>
-                  <TableCell>{user.lastLogin || "-"}</TableCell>
-                  <TableCell>
-                    <StatusBadge status={user.status as UserStatus} />
-                  </TableCell>
+          <TableBody className="divide-y divide-secondary/15">
+            {filteredUsers?.map((user) => (
+              <TableRow
+                key={user.id}
+              >
+                <TableCell className="px-2.5 flex items-center gap-2.5">
+                  <div className="bg-primary/50 w-fit rounded-full p-2">
+                    <UserIcon size={16} className="text-green-600" />
+                  </div>
+                  <div>
+                    <p className="font-medium text-left">{user.fullname}</p>
+                    <p className="font-light">{user.email}</p>
+                  </div>
+                </TableCell>
 
-                  <TableCell>
-                    <Button
-                      variant={"outline"}
-                      className="border-secondary ring-secondary hover:bg-secondary hover:text-background inline-flex cursor-pointer items-center gap-2 rounded-lg border px-3 py-2 text-xs capitalize focus:ring"
-                    >
-                      <MessageCircle size={16} />
-                      <span className="text-xs capitalize">Message user</span>
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              );
-            })}
+                <TableCell>{user.phoneNumber}</TableCell>
+                <TableCell>
+                  {user.location.lga} {user.location?.state}
+                </TableCell>
+                <TableCell>{user.source || "-"}</TableCell>
+                <TableCell>{user.lastLogin || "-"}</TableCell>
+
+                <TableCell>
+                  <StatusBadge status={user.status as UserStatus} />
+                </TableCell>
+
+                <TableCell>
+                  <Button
+                    variant="outline"
+                    className="border-secondary ring-secondary hover:bg-secondary hover:text-background inline-flex items-center gap-2 rounded-lg border px-3 py-2 text-xs capitalize"
+                  >
+                    <MessageCircle size={16} />
+                    Message user
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
-        <PaginationComponent />
       </div>
     </div>
-  );
-};
+      {/* <div className="w-full bg-secondary h-10"></div> */}
+    </>
+  )}
 
 export default AllUsers;
